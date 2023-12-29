eee796b594a334fde3fbe518c49bb9ee657f2542
Yaroslav <99386996+roninbean@users.noreply.github.com>
2023-12-30 01:08:40 +0200

    Delete lab4 directory

--- lab4/hello1.h
+++
@@ -1,7 +0,0 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/printk.h>
-#include <linux/list.h>
-#include <linux/ktime.h>
-#include <linux/slab.h>
-int print_hello(uint myParm);
